0a1b777c0e576b916432be02944bf8b811cf51dd
matteogeertsema <m.geertsema@st.hanze.nl>
2018-01-27 17:08:23 +0100

    test

---
+++ src/nl/hanze/parkeersimulator/model/TimeModel.java
@@ -0,0 +1,12 @@
+package nl.hanze.parkeersimulator.model;
+
+public class TimeModel {
+
+	private int day = 0;
+	private int hour = 0;
+	private int minute = 0;
+	
+	public void advanceTimeByOneMinute(){
+		
+	}
+}
